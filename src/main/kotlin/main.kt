781fcb47c725aebd11c7b80446770a8b5649918f
Suntrie <is.egorova@mail.ru>
2018-01-26 16:41:09 +0300

    Добавлено логирование в файл, исправлены ошибка синхронизации счётчиков(в случае, если язык проекта установлен, и это не java), ошибка с таймаутом.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,6 +10,10 @@ import java.nio.charset.Charset
 import java.util.HashMap
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.mainBody
+import java.util.logging.FileHandler
+import java.util.logging.Level
+import java.util.logging.Logger
+import java.util.logging.SimpleFormatter
 
 /**
  * Created by Neverland on 18.01.2018.
@@ -24,7 +28,12 @@ var clientInitialLimitedRequestTime = System.currentTimeMillis()
 
 val client = GitHubClient()
 
+var fileLogger: Logger? = null
+var fileHandler: FileHandler? = null
+
 class MyArgs(parser: ArgParser) {
+
+    val logger by parser.storing("logger's system path")
     val user by parser.storing("login for github authentication")
     val password by parser.storing("password for github authentication")
 
@@ -32,16 +41,30 @@ class MyArgs(parser: ArgParser) {
 
 fun main(args: Array<String>) {
 
-    println(TAG+"I'm starting now...")
-
     var parsedArgs: MyArgs? = null
 
     mainBody {
         parsedArgs = ArgParser(args).parseInto(::MyArgs)
     }
 
+   fileLogger = Logger.getLogger(TAG+"Log")
+
+    try {
+
+        fileHandler = FileHandler(parsedArgs!!.logger)
+        fileLogger!!.addHandler(fileHandler)
+        val formatter = SimpleFormatter()
+        fileHandler!!.formatter = formatter
+
+    } catch (e: SecurityException) {
+        e.printStackTrace()
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+
     val factory = ConnectionFactory()
     factory.host = "localhost"
+    factory.requestedHeartbeat = 0
     val connection = factory.newConnection()
     val channel = connection.createChannel()
 
@@ -66,52 +89,70 @@ fun sendData(connection: Connection, pageIterator: PageIterator<Repository>, sen
     val dataSendResult=sendDataBeforeTrigger(pageIterator, sendChannel)
 
     if (dataSendResult.first){
-        println(TAG+"Waiting for acknowledgment (100 picked repo's should be downloaded&stored).")
+        fileLogger!!.log(Level.INFO,"Waiting for acknowledgment (100 picked repo's should be downloaded&stored).")
         waitForDataConsumption(connection, sendChannel, pageIterator);
         return
     }
 
-    println(TAG+"Total number of repo's: $totalNumberOfRepos")
-    println(TAG+"Number of java repo's: $numberOfJavaRepositories")
+    fileLogger!!.log(Level.INFO,"Total number of repo's: $totalNumberOfRepos")
+    fileLogger!!.log(Level.INFO,"Number of java repo's: $numberOfJavaRepositories")
 
     sendChannel.basicPublish("", TASKS_QUEUE_NAME, null, "stop".toByteArray())
     sendChannel.close()
+    connection.close()
 
+    fileHandler!!.close()
 }
 
 var pagePickerCounter =0
 var totalNumberOfRepos=0
 
+var javaRepositoriesCounter=0
+
 private fun sendDataBeforeTrigger(pageIterator: PageIterator<Repository>,
                                   sendChannel: Channel): Pair<Boolean, Int> {
+
     while (pageIterator.hasNext()) {
 
-        println("Iteration: $pagePickerCounter, remaining rate: ${client.remainingRequests}")
+        fileLogger!!.log(Level.INFO,"Iteration: $pagePickerCounter, remaining rate: ${client.remainingRequests}")
 
         try {
 
             var tmp=pageIterator.next().withIndex()
 
             for ((index, repo) in tmp) {
-                println(TAG+"Index: $index, name: ${repo.name}")
+                fileLogger!!.log(Level.INFO,"Index: $index, name: ${repo.name}")
                 totalNumberOfRepos++
                 if ((repo.language == "java") || (repo.language == null)) {
+                    fileLogger!!.log(Level.INFO,"Java repository, number: $javaRepositoriesCounter")
+                    javaRepositoriesCounter++
 
                     sendChannel.basicPublish("", TASKS_QUEUE_NAME,
                             MessageProperties.PERSISTENT_BASIC, (repo.url + "/zipball").toByteArray())
                     numberOfJavaRepositories++;
-                    if ((index+1) % 100 == 0) {
-                        return Pair(true,index)
+                    if ((javaRepositoriesCounter) % 100 == 0) {
+                        return Pair(true,javaRepositoriesCounter)
                     }
                 }
             }
         } catch (e: NoSuchPageException) {
-            println(TAG+"Abuse/ rate limit handler processing.")
+            fileLogger!!.log(Level.INFO,"Abuse/ rate limit handler processing.")
             if ((e.cause as RequestException).status == 403) {
                 val sleepDuration = clientInitialLimitedRequestTime + 1000 * 60 * 60 - System.currentTimeMillis()
                 Thread.sleep(sleepDuration)
                 clientInitialLimitedRequestTime = System.currentTimeMillis()
-            } else throw Exception(TAG+"Connection was abandoned: " + e.message)
+            } else {
+                val exceptionMessage="Connection was abandoned: " + e.message;
+                fileLogger!!.log(Level.INFO,exceptionMessage)
+
+                val connection=sendChannel.connection
+                sendChannel.close()
+                connection.close()
+
+                fileHandler!!.close()
+
+                throw Exception(exceptionMessage)
+            }
         }
 
         pagePickerCounter++
@@ -137,7 +178,7 @@ fun waitForDataConsumption(connection: Connection,
 
             val message = String(body, Charset.forName("UTF-8"))
             if (message == "consumed") {
-
+                fileLogger!!.log(Level.INFO,"Got acknowledgment.")
                 sendData(connection, pageIterator, sendChannel)
                 responseChannel.close()
             }
